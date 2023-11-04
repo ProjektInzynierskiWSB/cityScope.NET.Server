44e08c0be7098189184b7a873420de1af0b5aed9
Bartosz Panic <bartosz.panic@gmail.com>
2023-11-04 09:02:16 +0100

    Change unit tests

--- cityScope.NET.Server.UnitTest/AnnouncementTests/CreateAnnouncementTest.cs
+++ cityScope.NET.Server.UnitTest/AnnouncementTests/CreateAnnouncementTest.cs
@@ -5,11 +5,6 @@ using cityScope.NET.Server.Application.Services.Interfaces;
 using cityScope.NET.Server.UnitTest.Mocks;
 using Moq;
 using Shouldly;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace cityScope.NET.Server.UnitTest.AnnouncementTests
 {
@@ -17,26 +12,28 @@ namespace cityScope.NET.Server.UnitTest.AnnouncementTests
     {
         private readonly Mock<IAnnouncementRepository> _mockRepository;
         private readonly Mock<IUserService> _mockUserService;
+        private readonly Mock<IPhotosService> _photosService;
 
         public CreateAnnouncementTest()
         {
             _mockRepository = RepositoryMocks.GetAnnouncementRepository();
             _mockUserService = RepositoryMocks.GetUserService();
+            _photosService = RepositoryMocks.GetPhotosService();
         }
 
         [Fact]
         public async Task ValidAnnouncement_AddedToRepo()
         {
-            var hanlder = new AnnouncementService(_mockRepository.Object, _mockUserService.Object);
+            var handler = new AnnouncementService(_mockRepository.Object, _mockUserService.Object, _photosService.Object);
             var allAnnouncementBeforeCount = (await _mockRepository.Object.GetAllAsync()).Count();
-            AnnouncementDto announcementDto = new()
+            AddAnnouncementDto announcementDto = new()
             {
                 Title = "Testvalid",
                 Description = "TestValid",
                 Price = 11.99m
             };
 
-            var response = await hanlder.AddAnnouncement(announcementDto);
+            var response = await handler.AddAnnouncement(announcementDto);
 
             var allAfter = await _mockRepository.Object.GetAllAsync();
 
@@ -47,17 +44,17 @@ namespace cityScope.NET.Server.UnitTest.AnnouncementTests
         [Fact]
         public async Task Not_ValidAnnouncement_TooLongTitle_101_Characters_NotAddedToRepo()
         {
-            var hanlder = new AnnouncementService(_mockRepository.Object, _mockUserService.Object);
+            var handler = new AnnouncementService(_mockRepository.Object, _mockUserService.Object, _photosService.Object);
             var allAnnouncementBeforeCount = (await _mockRepository.Object.GetAllAsync()).Count();
 
-            AnnouncementDto announcementDto = new()
+            AddAnnouncementDto announcementDto = new()
             {
                 Title = new string('*', 101),
                 Description = "Test",
                 Price = 11.99m
             };
 
-            var response = await hanlder.AddAnnouncement(announcementDto);
+            var response = await handler.AddAnnouncement(announcementDto);
 
             var allAfter = await _mockRepository.Object.GetAllAsync();
 
@@ -68,17 +65,17 @@ namespace cityScope.NET.Server.UnitTest.AnnouncementTests
         [Fact]
         public async Task Not_ValidAnnouncement_TooLongDescription_1001_Characters_NotAddedToRepo()
         {
-            var hanlder = new AnnouncementService(_mockRepository.Object, _mockUserService.Object);
+            var handler = new AnnouncementService(_mockRepository.Object, _mockUserService.Object, _photosService.Object);
             var allAnnouncementBeforeCount = (await _mockRepository.Object.GetAllAsync()).Count();
 
-            AnnouncementDto announcementDto = new()
+            AddAnnouncementDto announcementDto = new()
             {
                 Title = new string('*', 1001),
                 Description = "Test",
                 Price = 11.99m
             };
 
-            var response = await hanlder.AddAnnouncement(announcementDto);
+            var response = await handler.AddAnnouncement(announcementDto);
 
             var allAfter = await _mockRepository.Object.GetAllAsync();
 
@@ -89,17 +86,17 @@ namespace cityScope.NET.Server.UnitTest.AnnouncementTests
         [Fact]
         public async Task Not_ValidAnnouncement_PriceNotValid_NotAddedToRepo()
         {
-            var hanlder = new AnnouncementService(_mockRepository.Object, _mockUserService.Object);
+            var handler = new AnnouncementService(_mockRepository.Object, _mockUserService.Object, _photosService.Object);
             var allAnnouncementBeforeCount = (await _mockRepository.Object.GetAllAsync()).Count();
 
-            AnnouncementDto announcementDto = new()
+            AddAnnouncementDto announcementDto = new()
             {
                 Title = new string('*', 1001),
                 Description = "Test",
                 Price = 11.9999m
             };
 
-            var response = await hanlder.AddAnnouncement(announcementDto);
+            var response = await handler.AddAnnouncement(announcementDto);
 
             var allAfter = await _mockRepository.Object.GetAllAsync();
 
